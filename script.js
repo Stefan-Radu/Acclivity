e90191f431b1d8133ce5680ceb70e927ac508db4
Stefan Radu <stefan270101@gmail.com>
2020-04-20 16:09:40 +0300

    Clean version one
    * reformatted html
    * changed html ids / classes
    * written proper css
    * changed cursor

--- script.js
+++ script.js
@@ -3,14 +3,28 @@ function fixDpi(canvas) {
   let dpi = window.devicePixelRatio;
   let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
   let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
-  canvas.setAttribute('height', style_height * dpi);
-  canvas.setAttribute('width', style_width * dpi);
+  canvas.height = style_height * dpi;
+  canvas.width = style_width * dpi;
+}
+
+function getDist(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+
+function getAngle(x1, y1, x2, y2) {
+
+  let x = x2 - x1;
+  let y = y2 - y1;
+
+  alpha = Math.atan(tg = y / Math.abs(x));
+  if (x < 0) alpha = Math.PI - alpha;
+
+  return alpha;
 }
 
 function drawTriangle(context, x, y, w, h, alpha, out) {
 
   let ipp = Math.sqrt(w * w / 4 + h * h);
-  // console.log(ipp);
   let omega = Math.atan(w / (2 * h));
 
   if (out) context.globalCompositeOperation="destination-out";
@@ -32,86 +46,90 @@ function drawTriangle(context, x, y, w, h, alpha, out) {
   context.fill();
 }
 
-function getDist(x1, y1, x2, y2) {
-  return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+function clearCanvas(context, canvas) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
 }
-
-function getAngle(x1, y1, x2, y2) {
-
-  let x = x2 - x1;
-  let y = y2 - y1;
-
-  alpha = Math.atan(tg = y / Math.abs(x));
-  if (x < 0) alpha = Math.PI - alpha;
-
-  return alpha;
-}
-
-let out = false;
-let img1 = "url(\"https://i.pinimg.com/originals/de/04/42/de04423b9f942def5cffb86987e0806d.jpg\")";
-let img2 = "url()";
 
 window.onload = function () {
 
-  const canvas = document.getElementById("two");
-  fixDpi(canvas);
+  const drawCanvas = document.getElementById("drawCanvas");
+  const drawContext = drawCanvas.getContext("2d");
+  const previewCanvas = document.getElementById("previewCanvas");
+  const previewContext = previewCanvas.getContext("2d");
 
-  const context = canvas.getContext("2d");
+  fixDpi(drawCanvas);
+  fixDpi(previewCanvas);
 
-  let clearButton = this.document.getElementById("clear");
-
-  clearButton.addEventListener("click", () => {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+  document.getElementById("clear").addEventListener("click", () => {
+    clearCanvas(drawContext, drawCanvas);
   });
 
-  let imgButton = this.document.getElementById("clear2");
-  imgButton.addEventListener("click", () => {
-    let k = document.getElementById("one");
-    k.style.backgroundImage = img2;
-    [img1, img2] = [img2, img1];
+  let img = "", showImage = true;
+  let docBackground = document.getElementById("background");
+
+  document.getElementById("imgUrl").addEventListener("change", () => {
+    img = "url(\"" + document.getElementById("imgUrl").value + "\")";
+    if (showImage) docBackground.style.backgroundImage = img;
   });
 
-  let opButton = this.document.getElementById("clear3");
-  opButton.addEventListener("click", () => {
-    let k = this.document.getElementById("two");
-    if (k.style.opacity == 0.5) {
-      k.style.opacity = 1;
+  let docTogImg = document.getElementById("toggleImage"); 
+  docTogImg.addEventListener("click", () => {
+    showImage = !showImage;
+    if (showImage) {
+      docTogImg.innerText = "Hide Img";
+      docBackground.style.backgroundImage = img;
     }
     else {
-      k.style.opacity = 0.5;
+      docTogImg.innerText = "Show Img";
+      docBackground.style.backgroundImage = "url()";
     }
   });
 
-  let eraseButton = this.document.getElementById("clear4");
-  eraseButton.addEventListener("click", () => out = !out);
+  document.getElementById("opacity").addEventListener("click", () => {
+    if (drawCanvas.style.opacity == 0.5) drawCanvas.style.opacity = 1;
+    else drawCanvas.style.opacity = 0.5;
+  });
+
+  let erasing = false;
+  let docBrush = document.getElementById("toggleBrush");
+  docBrush.addEventListener("click", () => {
+    erasing = !erasing;
+    docBrush.innerText = erasing ? "Brush": "Eraser";
+  });
 
   let x = 0, y = 0, w = 50, h = 50;
   let prevAlpha = 0, curAlpha = 0;
   let alpha = Math.PI / 2, drawing = false;
 
-  widthSlider = this.document.getElementById("slide1");
-  widthSlider.addEventListener("change", () => w = widthSlider.value);
+  widthSlider = document.getElementById("wSlider");
+  widthSlider.addEventListener("mousemove", () => {
+    w = widthSlider.value;
+    document.getElementById("brushWidth").innerText = w;
+  });
 
-  heightSlider = this.document.getElementById("slide2");
-  heightSlider.addEventListener("change", () => h = heightSlider.value);
+  heightSlider = document.getElementById("hSlider");
+  heightSlider.addEventListener("mousemove", () => {
+    h = heightSlider.value;
+    document.getElementById("brushHeight").innerText = h;
+  });
 
-  canvas.addEventListener("mousedown", () => {
+  drawCanvas.addEventListener("mousedown", () => {
     drawing = true;
   });
 
-  canvas.addEventListener("mousemove", e => {
-    if (this.getDist(x, y, e.offsetX, e.offsetY) > 7.0) {
-      curAlpha = this.getAngle(x, y, e.offsetX, e.offsetY);
+  drawCanvas.addEventListener("mousemove", e => {
+    if (getDist(x, y, e.offsetX, e.offsetY) > 7.0) {
+      curAlpha = getAngle(x, y, e.offsetX, e.offsetY);
       alpha -= (curAlpha - prevAlpha);
       prevAlpha = curAlpha;
       x = e.offsetX;
       y = e.offsetY;
     }
 
-    if (drawing) drawTriangle(context, e.offsetX, e.offsetY, w, h, alpha, out);
+    if (drawing) drawTriangle(drawContext, e.offsetX, e.offsetY, w, h, alpha, erasing);
   });
 
-  canvas.addEventListener("mouseup", () => {
+  drawCanvas.addEventListener("mouseup", () => {
     drawing = false;
   });
 }
